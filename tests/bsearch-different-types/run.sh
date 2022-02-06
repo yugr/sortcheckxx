8aacec284ddaa94ed6f6b83c05a6a5b53d387635
Yuri Gribov <tetra2005@gmail.com>
2022-02-06 15:33:10 +0300

    Support functor-less binary_search better.

--- tests/bsearch-different-types/run.sh
+++ tests/bsearch-different-types/run.sh
@@ -15,11 +15,28 @@ set -eu
 cd $(dirname $0)
 
 ROOT=$PWD/../..
+PATH=$ROOT/scripts:$PATH
 
-$ROOT/bin/SortChecker repro.cc --
-if grep -q sortcheck repro.cc; then
-  echo >&2 'Unexpected modifications (not handled yet)'
-#  exit 1
+CXXFLAGS='-Wall -Wextra -Werror -g'
+
+export SORTCHECK_ABORT=0
+
+c++ $CXXFLAGS repro.cc
+if ! ./a.out > test.log 2>&1; then
+  echo >&2 'Valid test did not run as expected'
+  cat test.log >&2
+  exit 1
+fi
+
+c++ $CXXFLAGS -DINVALID repro.cc
+if ./a.out > test.log 2>&1; then
+  echo >&2 'Test did not fail as expected'
+  exit 1
+fi
+if ! diff -q repro.ref test.log; then
+  echo >&2 'Test did not produce expected output:'
+  diff repro.ref test.log >&2
+  exit 1
 fi
 
 echo SUCCESS
